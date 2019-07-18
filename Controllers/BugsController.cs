423a230b301a81a7b0d157bb1968060ad4244d8e
ben <bholsinger@gmail.com>
2019-07-18 16:07:17 -0600

    finished CRUD

---
+++ Controllers/BugsController.cs
@@ -0,0 +1,103 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using bugserver.Models;
+using bugserver.Repositories;
+using Microsoft.AspNetCore.Mvc;
+
+namespace bugserver.Controllers
+{
+  [Route("api/[controller]")]
+  [ApiController]
+  public class BugsController : ControllerBase
+  {
+    private readonly BugRepository _repo;
+
+    public BugsController(BugRepository repo)
+    {
+      _repo = repo;
+    }
+    // GET api/values
+    [HttpGet]
+    public ActionResult<IEnumerable<Bug>> Get()
+    {
+      try
+      {
+        return Ok(_repo.GETALL());
+
+      }
+      catch (Exception e)
+      {
+        return BadRequest(e);
+
+      }
+
+    }
+
+    // GET api/values/5
+    [HttpGet("{id}")]
+    public ActionResult<Bug> Get(int id)
+    {
+      try
+      {
+        return Ok(_repo.GetById(id));
+
+      }
+      catch (Exception e)
+      {
+
+        return BadRequest(e);
+      }
+
+    }
+
+    // POST api/values
+    [HttpPost]
+    public ActionResult<Bug> Post([FromBody] string value)
+    {
+      try
+      {
+        return Ok(_repo.Create(value));
+      }
+      catch (Exception e)
+      {
+        return BadRequest(e);
+      }
+
+
+
+    }
+
+    // PUT api/values/5
+    [HttpPut("{id}")]
+    public ActionResult<Bug> Put(int id, [FromBody] Bug value)
+    {
+      try
+      {
+        value.Id = id;
+        return Ok(_repo.Update(value));
+      }
+      catch (Exception e)
+      {
+
+        return BadRequest(e);
+      }
+    }
+
+    // DELETE api/values/5
+    [HttpDelete("{id}")]
+    public ActionResult<string> Delete(int id)
+    {
+      try
+      {
+        return Ok(_repo.Delete(id));
+      }
+      catch (Exception e)
+      {
+
+        return BadRequest(e);
+      }
+    }
+  }
+}
--- Repositories/BugRepository.cs
+++ Repositories/BugRepository.cs
@@ -1,4 +1,8 @@
+using System;
+using System.Collections.Generic;
 using System.Data;
+using bugserver.Models;
+using Dapper;
 
 namespace bugserver.Repositories
 {
@@ -9,6 +13,57 @@ namespace bugserver.Repositories
     public BugRepository(IDbConnection db)
     {
       _db = db;
+    }
+
+    public IEnumerable<Bug> GETALL()
+    {
+      return _db.Query<Bug>("SELECT * FROM bugs");
+
+    }
+
+    public Bug GetById(int id)
+    {
+      string query = "SELECT * FROM bugs WHERE id = @id";
+      Bug bug = _db.QueryFirstOrDefault<Bug>(query, new { id });
+      if (bug == null) throw new Exception("Invalid Id");
+      return bug;
+    }
+
+    public Bug Create(Bug value)
+    {
+      string query = @"
+      INSERT INTO bugs (author, title, details)
+      VALUES (@Author, @Title, @Description);
+      SELECT LAST_INSERT_ID();
+      ";
+      int id = _db.ExecuteScalar<int>(query, value);
+      value.Id = id;
+      return value;
+    }
+
+    public Bug Update(Bug value)
+    {
+      string query = @"
+      UPDATE bus 
+      SET 
+         author = @Author,
+         title = @Title,
+         details = @Description
+         WHERE ID = @Id;
+         SELECT * FROM bugs WHERE id = @Id";
+      return _db.QueryFirstOrDefault<Bug>(query, value);
+
+
+
+    }
+
+    public string Delete(int id)
+    {
+      string query = "DELETE FROM bugs WHERE id = @Id";
+      int changedRows = _db.Execute(query, new { id });
+      if (changedRows < 1) throw new Exception("Invalid Id");
+      return "Successfully Deleed Bug";
+
     }
   }
 }
